de3e04d6a02a6941f864412dca906d31ae7ea2f2
yanfer <yanfer.daniel@gmail.com>
2024-10-05 06:47:41 -0600

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Write from './pages/Write';
+import SinglePost from './pages/SinglePost';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/single-post',
+    element: <SinglePost />,
+  },
+  {
+    path: '/write',
+    element: <Write />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
